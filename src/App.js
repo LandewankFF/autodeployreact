38141a2603aaf702a11a43dc4e7b999dc9bc55ce
LandewankFF <landewankfahrezaf@gmail.com>
2024-05-09 21:34:02 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -10,13 +10,14 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="App-link text-2xl text-blue-500"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Auto Deployment with Concept CI/CD
         </a>
+        <h1 className="text-3xl font-bold underline text-orange-500">Hello Tailwind!</h1>
       </header>
     </div>
   );
