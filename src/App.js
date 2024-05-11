babcadede7e8c9fec3fa119afd778d89c69d6aa7
Landewank Fahreza Firdaus <103626399+LandewankFF@users.noreply.github.com>
2024-05-11 08:40:43 +0700

    firx : tes pipeline success auto or not?

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          success setup ci cd pipeline on GitHub Action
         </a>
       </header>
     </div>
